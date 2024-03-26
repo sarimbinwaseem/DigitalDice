e0135da4db8932166217a42b5663ba9f90d1b4e3
sarimbinwaseem <sarimbinwaseem2000@gmail.com>
2024-03-26 08:55:15 +0500

    Code formatted

--- newmain.py
+++ newmain.py
@@ -4,35 +4,35 @@ from dice import roll, afterRoll
 
 
 class Ui_Dialog(QtWidgets.QDialog):
-    """UI functions of Digital Dice"""
+	"""UI functions of Digital Dice"""
 
-    def __init__(self) -> None:
-        super(Ui_Dialog, self).__init__()
+	def __init__(self) -> None:
+		super(Ui_Dialog, self).__init__()
 
-        uic.loadUi(os.path.join("GUI", "main.ui"), self)
-        self.setWindowTitle("Digital Dice")
+		uic.loadUi(os.path.join("GUI", "main.ui"), self)
+		self.setWindowTitle("Digital Dice")
 
-        self.numbers = [1, 2, 3, 4, 5, 6]
+		self.numbers = [1, 2, 3, 4, 5, 6]
 
-        self.player1Roll.clicked.connect(lambda: self.main_roll(self.listWidget))
-        self.player2Roll.clicked.connect(lambda: self.main_roll(self.listWidget_2))
-        self.player3Roll.clicked.connect(lambda: self.main_roll(self.listWidget_3))
-        self.player4Roll.clicked.connect(lambda: self.main_roll(self.listWidget_4))
+		self.player1Roll.clicked.connect(lambda: self.main_roll(self.listWidget))
+		self.player2Roll.clicked.connect(lambda: self.main_roll(self.listWidget_2))
+		self.player3Roll.clicked.connect(lambda: self.main_roll(self.listWidget_3))
+		self.player4Roll.clicked.connect(lambda: self.main_roll(self.listWidget_4))
 
-    def main_roll(self, listWidget) -> list:
-    	"""Rolls and update the list widget with new entry"""
+	def main_roll(self, listWidget) -> list:
+		"""Rolls and update the list widget with new entry"""
 
-        result = roll(self.numbers)
-        print(result)
+		result = roll(self.numbers)
+		print(result)
 
-        listWidget.addItem(afterRoll(result))
-        listWidget.scrollToBottom()
+		listWidget.addItem(afterRoll(result))
+		listWidget.scrollToBottom()
 
 
 if __name__ == "__main__":
-    import sys
+	import sys
 
-    app = QtWidgets.QApplication(sys.argv)
-    window = Ui_Dialog()
-    window.show()
-    app.exec()
+	app = QtWidgets.QApplication(sys.argv)
+	window = Ui_Dialog()
+	window.show()
+	app.exec()
